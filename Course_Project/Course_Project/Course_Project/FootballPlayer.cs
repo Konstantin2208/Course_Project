37a62e6c3408f42ed93a2f3945ef04bfd9ea3f7f
Konstantin2208 <alexsandrovkonstantin@gmail.com>
2023-05-25 18:00:22 +0300

    new changes

---
+++ Course_Project/Course_Project/Course_Project/FootballPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Course_Project
+{
+    public class FootballPlayer : Person
+    {
+        public int Number { get; set; }
+        public double Height { get; set; }
+        public FootballPlayer(string name, int age) : base(name, age)
+        {
+        }
+        public override string ToString()
+        {
+            return $"FootballPlayer{{number ={ Number}, height ={ Height},age ={ GetAge()}, name ={ GetName()}}}" ;
+        }
+    }
+}
--- Course_Project/Course_Project/Course_Project/Person.cs
+++ Course_Project/Course_Project/Course_Project/Person.cs
@@ -8,14 +8,13 @@ namespace Course_Project
 {
     public class Person
     {
-        private string name;
-        private int age;
         public Person(string name, int age)
         {
-            this.name = name;
-            this.age = age;
+            this.Name = name;
+            this.Age = age;
         }
-       
+        public string Name { get; set; }
+        public int Age { get; set; }
 
     }
 }
